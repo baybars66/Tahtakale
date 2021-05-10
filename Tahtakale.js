577469b0aa3535830ec1bb38b672c084529405ba
Baybars ÖNAL <bay66bars@gmail.com>
2021-05-10 11:33:28 +0300

    first

---
+++ Tahtakale.js
@@ -0,0 +1,22 @@
+const io = require('socket.io')(8666);
+  
+io.on('connection', function(socket){
+  var clientIp = socket.request.connection.remoteAddress;
+      console.log(clientIp);
+      
+   socket.on('chat message', function(msg){
+     io.emit('chat message', msg);
+     console.log(msg);
+     
+       });
+
+   socket.on('ip message', function(){
+      var clientIp = socket.request.connection.remoteAddress;
+      console.log(clientIp);
+      io.emit('chat message', clientIp);
+      
+      });
+
+
+
+});
